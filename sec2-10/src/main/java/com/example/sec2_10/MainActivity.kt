e2eaf5448a61db22b0a7ec59e568dd1dd7400415
Naoyuki Sugi <bnsgny@gmail.com>
2021-02-24 13:00:22 +0900

    prepare

---
+++ sec2-10/src/main/java/com/example/sec2_10/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sec2_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.sec2_10.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val student = getStudent()
+        binding.nameText.text = student.name
+        binding.emailText.text = student.email
+    }
+
+    private fun getStudent(): Student {
+        return Student(1, "Alex", "alex@gmail.com")
+    }
+}
