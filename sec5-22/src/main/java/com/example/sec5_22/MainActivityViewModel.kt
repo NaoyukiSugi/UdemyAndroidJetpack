cf1e6bf3b5eafbcf0052de2d51a6e10c3a426dce
Naoyuki Sugi <bnsgny@gmail.com>
2021-02-25 18:23:34 +0900

    two way data binding

--- sec5-22/src/main/java/com/example/sec5_22/MainActivityViewModel.kt
+++ sec5-22/src/main/java/com/example/sec5_22/MainActivityViewModel.kt
@@ -1,13 +1,10 @@
 package com.example.sec5_22
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    private val userName = MutableLiveData<String>()
-    val userNameData: LiveData<String>
-        get() = userName
+    val userName = MutableLiveData<String>()
 
     init {
         userName.value = "Frank"
