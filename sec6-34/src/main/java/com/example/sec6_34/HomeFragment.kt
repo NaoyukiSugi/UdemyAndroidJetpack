05d64cff6f3723bfc699c0c805b992fac87be974
shishamo <bnsgny@gmail.com>
2021-02-26 16:57:47 +0900

    solve (#28)

--- sec6-34/src/main/java/com/example/sec6_34/HomeFragment.kt
+++ sec6-34/src/main/java/com/example/sec6_34/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.sec6_34.databinding.FragmentHomeBinding
 
 /**
@@ -19,6 +20,12 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.signUpButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_nameFragment)
+        }
+        binding.termsButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_termsFragment)
+        }
         return binding.root
     }
 }
