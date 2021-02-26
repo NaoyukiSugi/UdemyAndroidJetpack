5e7fa526030bb28e150d317d83dd26260af494a9
Naoyuki Sugi <bnsgny@gmail.com>
2021-02-26 19:47:26 +0900

    implement

--- sec7-36/src/main/java/com/example/sec7_36/MainActivity.kt
+++ sec7-36/src/main/java/com/example/sec7_36/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sec7_36
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,6 +25,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         my_recycler_view.setBackgroundColor(Color.YELLOW)
         my_recycler_view.layoutManager = LinearLayoutManager(this)
-        my_recycler_view.adapter = MyRecyclerViewAdapter(fruitsList)
+        my_recycler_view.adapter =
+            MyRecyclerViewAdapter(fruitsList) { selectedFruitItem: Fruit ->
+                listItemClicked(
+                    selectedFruitItem
+                )
+            }
+    }
+
+    private fun listItemClicked(fruit: Fruit) {
+        Toast.makeText(
+            this@MainActivity,
+            "Supplier name is ${fruit.supplier}", Toast.LENGTH_LONG
+        ).show()
     }
 }
