97fe4ad4622af5acfb28468a573a547172428732
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-02 12:31:08 +0900

    solve

--- sec8-42/src/main/java/com/example/sec8_42/MainActivity.kt
+++ sec8-42/src/main/java/com/example/sec8_42/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.sec8_42
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.i("MyTag", "Hello from ${Thread.currentThread().name}")
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.i("MyTag", "Hello from ${Thread.currentThread().name}")
+        }
     }
 }
