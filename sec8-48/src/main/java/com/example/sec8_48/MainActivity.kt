ceb4e4267346579091b54a5f7e3859dc4436adfd
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-02 14:57:26 +0900

    non-parrallel implement

--- sec8-48/src/main/java/com/example/sec8_48/MainActivity.kt
+++ sec8-48/src/main/java/com/example/sec8_48/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.sec8_48
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(IO).launch {
+            Log.i("MyTag", "Calculation started....")
+            val stock1 = getStock1()
+            val stock2 = getStock2()
+            val total = stock1 + stock2
+            Log.i("MyTag", "Total is $total")
+        }
     }
+}
+
+private suspend fun getStock1(): Int {
+    delay(10000)
+    Log.i("MyTag", "stock 1 returned")
+    return 55000
+}
+
+private suspend fun getStock2(): Int {
+    delay(8000)
+    Log.i("MyTag", "stock 2 returned")
+    return 35000
 }
