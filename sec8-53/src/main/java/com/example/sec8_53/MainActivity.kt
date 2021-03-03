59bde5895171d555342582e8c428dc1cd82e886f
shishamo <bnsgny@gmail.com>
2021-03-03 17:06:58 +0900

    implement (#47)

--- sec8-53/src/main/java/com/example/sec8_53/MainActivity.kt
+++ sec8-53/src/main/java/com/example/sec8_53/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.sec8_53
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+//        mainActivityViewModel.getUsers()
+        mainActivityViewModel.users.observe(this, Observer { myUsers ->
+            myUsers.forEach {
+                Log.i("MyTag", "name is ${it.name}")
+            }
+        })
     }
 }
