4f4d90c959dbd717f5c7c8ae89bc02c4f9639d7f
shishamo <bnsgny@gmail.com>
2021-03-04 12:58:18 +0900

    implement (#51)

--- sec13-92/src/main/java/com/example/sec13_92/MemoryCard.kt
+++ sec13-92/src/main/java/com/example/sec13_92/MemoryCard.kt
@@ -3,7 +3,7 @@ package com.example.sec13_92
 import android.util.Log
 import javax.inject.Inject
 
-class MemoryCard @Inject constructor() {
+class MemoryCard {
     init {
         Log.i("MYTAG", "Memory Card Constructed")
     }
