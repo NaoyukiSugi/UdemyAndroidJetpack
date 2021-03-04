88f99ee6fab6d0d9f67d6021918c5e552eadeff2
shishamo <bnsgny@gmail.com>
2021-03-04 15:03:09 +0900

    update (#52)

---
+++ sec13-92/src/main/java/com/example/sec13_92/NCBatteryModule.kt
@@ -0,0 +1,10 @@
+package com.example.sec13_92
+
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class NCBatteryModule {
+    @Binds
+    abstract fun bindsNCBattery(nickelCadmiumBattery: NickelCadmiumBattery): Battery
+}
