3d51557c2356ed19f075a68f3dcf6e2c5b3797ba
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-04 15:02:28 +0900

    update

--- sec13-92/src/main/java/com/example/sec13_92/SmartPhoneComponent.kt
+++ sec13-92/src/main/java/com/example/sec13_92/SmartPhoneComponent.kt
@@ -2,7 +2,7 @@ package com.example.sec13_92
 
 import dagger.Component
 
-@Component(modules = [MemoryCardModule::class])
+@Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
     fun getSmartPhone(): SmartPhone
 }
