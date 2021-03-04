5717642530862b8a07fb76de03b34ad764a75d8e
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-04 15:41:43 +0900

    update

--- sec13-92/src/main/java/com/example/sec13_92/MainActivity.kt
+++ sec13-92/src/main/java/com/example/sec13_92/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.sec13_92
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var smartPhone: SmartPhone
+    @Inject
+    lateinit var smartPhone: SmartPhone
+
+//    @Inject
+//    lateinit var memoryCard: MemoryCard
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         DaggerSmartPhoneComponent
-            .create()
-            .getSmartPhone()
-            .makeACallWithRecording()
+                .create()
+                .inject(this)
+        smartPhone.makeACallWithRecording()
+//        memoryCard.getSpaceAvailablity()
     }
 }
