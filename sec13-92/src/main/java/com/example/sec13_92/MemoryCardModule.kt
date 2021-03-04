f57d1f09c13da0732e08d463034576bcdc1e65f7
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-04 12:57:41 +0900

    implement

---
+++ sec13-92/src/main/java/com/example/sec13_92/MemoryCardModule.kt
@@ -0,0 +1,13 @@
+package com.example.sec13_92
+
+import dagger.Module
+import dagger.Provides
+
+@Module
+class MemoryCardModule {
+
+    @Provides
+    fun providesMemoryCard(): MemoryCard {
+        return MemoryCard()
+    }
+}
