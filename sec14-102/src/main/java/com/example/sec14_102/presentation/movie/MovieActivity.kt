b9912f5a5164e9b1c95b762030a2c6f2a30c1784
shishamo <bnsgny@gmail.com>
2021-03-10 20:20:21 +0900

    update (#67)

--- sec14-102/src/main/java/com/example/sec14_102/presentation/movie/MovieActivity.kt
+++ sec14-102/src/main/java/com/example/sec14_102/presentation/movie/MovieActivity.kt
@@ -2,6 +2,9 @@ package com.example.sec14_102.presentation.movie
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -50,6 +53,36 @@ class MovieActivity : AppCompatActivity() {
             } else {
                 binding.movieProgressbar.visibility = View.GONE
                 Toast.makeText(applicationContext, "No data available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.update, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateMovies() {
+        binding.movieProgressbar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this, Observer {
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressbar.visibility = View.GONE
+            } else {
+                binding.movieProgressbar.visibility = View.GONE
             }
         })
     }
