e83b4b4c6400e0f782032abd5bc118c0bb46ff79
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-11 17:11:46 +0900

    update

--- sec25-230/src/main/java/com/example/sec25_230/service/MovieDataService.java
+++ sec25-230/src/main/java/com/example/sec25_230/service/MovieDataService.java
@@ -9,4 +9,7 @@ import retrofit2.http.Query;
 public interface MovieDataService {
     @GET("movie/popular")
     Call<MovieDBResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/popular")
+    Call<MovieDBResponse> getPopularMoviesWithPaging(@Query("api_key") String apiKey, @Query("api_key") long page);
 }
