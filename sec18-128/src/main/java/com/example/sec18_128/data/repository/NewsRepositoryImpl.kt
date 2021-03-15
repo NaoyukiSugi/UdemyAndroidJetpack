0f18113edbec0d8acdbc96ee5c45f95b0be05fba
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-15 17:51:49 +0900

    update

---
+++ sec18-128/src/main/java/com/example/sec18_128/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.example.sec18_128.data.repository
+
+import com.example.sec18_128.data.model.APIResponse
+import com.example.sec18_128.data.model.Article
+import com.example.sec18_128.data.repository.dataSource.NewsRemoteDataSource
+import com.example.sec18_128.data.util.Resource
+import com.example.sec18_128.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+
+class NewsRepositoryImpl(
+    private val newsRemoteDataSource: NewsRemoteDataSource
+) : NewsRepository {
+    override suspend fun getNewsHeadlines(): Resource<APIResponse> {
+        return responseToResource(newsRemoteDataSource.getTopHeadlines())
+    }
+
+    override suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveNews(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNews(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSavedNews(): Flow<List<Article>> {
+        TODO("Not yet implemented")
+    }
+
+    private fun responseToResource(response: Response<APIResponse>): Resource<APIResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { result ->
+                return Resource.Success(result)
+
+            }
+        }
+        return Resource.Error(response.message())
+    }
+}
