f72e13e91368dd7dfed4bfcc7bf90efa4b2e025b
shishamo <bnsgny@gmail.com>
2021-03-15 13:13:28 +0900

    update (#84)

---
+++ sec18-128/src/main/java/com/example/sec18_128/domain/usecase/GetSavedNewsUseCase.kt
@@ -0,0 +1,6 @@
+package com.example.sec18_128.domain.usecase
+
+import com.example.sec18_128.domain.repository.NewsRepository
+
+class GetSavedNewsUseCase(private val newsRepository: NewsRepository) {
+}
