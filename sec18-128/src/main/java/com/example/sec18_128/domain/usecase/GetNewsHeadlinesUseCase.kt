e3142d90ce7c2d159581cbefa4831280595c4688
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-16 11:51:12 +0900

    update

--- sec18-128/src/main/java/com/example/sec18_128/domain/usecase/GetNewsHeadlinesUseCase.kt
+++ sec18-128/src/main/java/com/example/sec18_128/domain/usecase/GetNewsHeadlinesUseCase.kt
@@ -4,5 +4,5 @@ import com.example.sec18_128.domain.repository.NewsRepository
 
 class GetNewsHeadlinesUseCase(private val newsRepository: NewsRepository) {
 
-    suspend fun execute() = newsRepository.getNewsHeadlines()
+    suspend fun execute(country: String, page: Int) = newsRepository.getNewsHeadlines(country, page)
 }
