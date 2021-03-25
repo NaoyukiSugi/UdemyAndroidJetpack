d0ced3eb55e8063a5f4f0c3bb55fdd5d1402fad7
shishamo <bnsgny@gmail.com>
2021-03-25 16:11:22 +0900

    update (#105)

--- sec10-70/src/main/java/com/example/sec10_70/MainActivity.kt
+++ sec10-70/src/main/java/com/example/sec10_70/MainActivity.kt
@@ -2,35 +2,32 @@ package com.example.sec10_70
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.text_view
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val retService = RetrofitInstance
+        retService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
+        getRequestWithQueryParameters()
+        getRequestWithPathParameters()
+    }
+
+    private fun getRequestWithQueryParameters() {
         val responseLiveData: LiveData<Response<Albums>> = liveData {
             val response = retService.getSortedAlbums(3)
             emit(response)
         }
-
-        // path parameter example
-        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
-            val response: Response<AlbumsItem> = retService.getAlbum(3)
-            emit(response)
-        }
-        pathResponse.observe(this, Observer {
-            val title = it.body()?.title
-            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
-        })
 
         responseLiveData.observe(this, Observer {
             val albums = it.body()?.listIterator()
@@ -43,6 +40,18 @@ class MainActivity : AppCompatActivity() {
                     text_view.append(result)
                 }
             }
+        })
+    }
+
+    private fun getRequestWithPathParameters() {
+        // path parameter example
+        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response: Response<AlbumsItem> = retService.getAlbum(3)
+            emit(response)
+        }
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
         })
     }
 }
--- sec10-70/src/main/java/com/example/sec10_70/RetrofitInstance.kt
+++ sec10-70/src/main/java/com/example/sec10_70/RetrofitInstance.kt
@@ -1,15 +1,28 @@
 package com.example.sec10_70
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
+
     companion object {
         val BASE_URL = "http://jsonplaceholder.typicode.com/"
+
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder().apply {
+            addInterceptor(interceptor)
+        }.build()
+
         fun getRetrofitInstance(): Retrofit {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
