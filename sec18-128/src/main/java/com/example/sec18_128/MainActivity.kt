4aee8c0226b5d6188a4139899d84bef10d809d76
Naoyuki Sugi <bnsgny@gmail.com>
2021-03-26 16:59:35 +0900

    update

--- sec18-128/src/main/java/com/example/sec18_128/MainActivity.kt
+++ sec18-128/src/main/java/com/example/sec18_128/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.sec18_128
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.sec18_128.databinding.ActivityMainBinding
 import com.example.sec18_128.presentation.adapter.NewsAdapter
 import com.example.sec18_128.presentation.viewmodel.NewsViewModel
 import com.example.sec18_128.presentation.viewmodel.NewsViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,9 +26,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bnvNews.setupWithNavController(
-            fragment.findNavController()
-        )
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
         viewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
     }
 }
